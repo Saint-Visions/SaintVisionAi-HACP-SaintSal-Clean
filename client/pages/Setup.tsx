129468a6fa2d3d9004a60eef01a42f3a1cfc57d6
Saint-Visions <ryan@saintvisiongroup.com>
2025-07-16 02:17:51 -0700

    Add initial Setup page component

---
+++ client/pages/Setup.tsx
@@ -0,0 +1,8 @@
+export default function Setup() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-white text-3xl">
+      Setup Page is Live.
+    </div>
+  );
+}
+
